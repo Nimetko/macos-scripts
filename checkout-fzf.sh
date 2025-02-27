f38ef4fb623de2e70cbbea706c8e81913e7db24a
Jan Borovsky <jan.borovsky@consultant.lego.com>
2025-02-27 08:36:21 +0100

    improve fzf checkout script. Add -r parameter to checkout remote branches

--- checkout-fzf.sh
+++ checkout-fzf.sh
@@ -1,2 +1,13 @@
-# git checkout $(git branch -r | fzf)
-git checkout $(git branch | fzf)
+#!/bin/bash
+
+# Check if the -r option is provided
+if [[ "$1" == "-r" ]]; then
+    # Get the remote branch to checkout from
+    remote_branch=$(git branch -r | sed 's/origin\///' | sed 's/^ *//g' | fzf)
+
+    echo "Selected branch $remote_branch"
+    git checkout "$remote_branch"
+else
+    # Checkout local branch
+    git checkout $(git branch | fzf)
+fi
