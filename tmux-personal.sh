4e70bf2edf40a5efdcea3ae1b9bbddaf9f7df059
Jan Borovsky <jan.borovsky@consultant.lego.com>
2025-03-26 15:39:11 +0100

    change tmux-personal sh

--- tmux-personal.sh
+++ tmux-personal.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-tmux neww bash -c "nvim ~/x/personal/legoTodoTickets"
+session=~/x/personal
+
+if [[ -z $session ]]; then
+    exit 0
+fi
+
+session_name=$(basename "$session" | tr . _)
+
+# If we're inside tmux and already in the desired session, do nothing
+if [[ -n $TMUX ]] && [[ $(tmux display-message -p '#S') == "$session_name" ]]; then
+    exit 0
+fi
+
+if ! tmux has-session -t "$session_name" 2> /dev/null; then 
+    tmux new-session -s "$session_name" -c "$session" -d
+fi
+
+tmux switch-client -t "$session_name"
+#
+
+# mine, same as on windows
+echo "session: $session"
+cd session
+
+# tmux neww bash -c "nvim ~/x/personal/legoTodoTickets"
